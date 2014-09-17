a8d0c312744514638aa116f2f74cce1626453da2
Masatoshi Teruya <mah0x211@gmail.com>
2014-09-17 18:45:19 +0900

    change: test.lua has been updated.

--- test.lua
+++ test.lua
@@ -2,33 +2,41 @@ local geo = require('geo');
 
 local precision = 16;
 local incr = 0.1;
-local dlat, dlon, latlon, hash, result;
+local LAT_MAX = 89.9;
+local LON_MAX = 179.9;
+local count = 0;
+local lat, lon, hash, dlat, dlon, diff, result;
 
-for lat = -89.9, 89.9, incr do
-    for lon = -179.9, 179.9, incr do
+for lat = -LAT_MAX, LAT_MAX, incr do
+    for lon = -LON_MAX, LON_MAX, incr do
+        count = count + 1;
         hash = geo.encode( lat, lon, precision );
-        latlon = geo.decode( hash );
+        dlat, dlon = geo.decode( hash );
         -- check round off error
-        dlat = math.abs( lat - latlon.lat );
-        dlon = math.abs( lon - latlon.lon );
-        if dlat < incr and dlon < incr then
+        diff = {
+            lat = math.abs( lat - dlat ),
+            lon = math.abs( lon - dlon )
+        };
+        if diff.lat < incr and diff.lon < incr then
+            --[[
+            print(
+                ('OK %4d:%4d -> %s precision %d -> %4d:%4d'):format(
+                    lat, lon, hash, precision, dlat, dlon
+                )
+            );
+            --]]
             result = 'OK';
         else
             result = 'FAIL';
-        end
-        
-        if result == 'FAIL' then 
-            print( 
-                '( ' .. tostring( lat ) .. ', ' .. tostring( lon ) .. ', ' .. tostring( precision ) .. ' ) -> ' ..
-                hash .. ' -> ' .. 
-                '( ' .. tostring( latlon.lat ) .. ', ' .. tostring( latlon.lon ) .. ' ) -> ' ..
-                'margin: ' .. tostring( dlat ) .. ', ' .. tostring( dlon ) .. ' -> ' ..
-                result
+            print(
+                ('( %d, %d, %d ) -> %s -> ( %d, %d ) -> margin: %d, %d -> %s')
+                :format( lat, lon, precision, hash, diff.lat, diff.lon, dlat,
+                         dlon, result )
             );
-            return; 
+            return;
         end
     end
 end
 
-print( 'test finished' );
+print( ('test %d position finished'):format( count ) );
 
