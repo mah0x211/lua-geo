2e1254fea3857f806c5a37b79e6a734fe979fbcd
Masatoshi Teruya <mah0x211@gmail.com>
2017-11-20 15:08:03 +0900

    Refactor src/quadkeys.c

--- src/quadkeys.c
+++ src/quadkeys.c
@@ -52,91 +52,88 @@ static inline double getclip( double n, double minValue, double maxValue )
 
 // Determines the map width and height (in pixels) at a specified level
 // of detail.
-// levelOfDetail: Level of detail, from 1 (lowest detail) to 23 (highest detail).
+// lv: Level of detail, from 1 (lowest detail) to 23 (highest detail).
 // returns: The map width and height in pixels.
-static inline unsigned int getmapsize( int levelOfDetail )
+static inline unsigned int getmapsize( int lv )
 {
-    return 256 << levelOfDetail;
+    return 256 << lv;
 }
 
 
 // Converts a point from latitude/longitude WGS-84 coordinates (in degrees)
 // into pixel XY coordinates at a specified level of detail.
-// latitude: latitude of the point, in degrees.
-// longitude: longitude of the point, in degrees.
-// levelOfDetail: from 1 (lowest detail) to 23 (highest detail)
-// pixelX: Output parameter receiving the X coordinate in pixels.
-// pixelY: Output parameter receiving the Y coordinate in pixels.
-static void LatLongToPixelXY( double latitude, double longitude,
-                              int levelOfDetail, int *pixelX, int *pixelY )
+// lat: latitude of the point, in degrees.
+// lon: longitude of the point, in degrees.
+// lv: from 1 (lowest detail) to 23 (highest detail)
+// px: Output parameter receiving the X coordinate in pixels.
+// py: Output parameter receiving the Y coordinate in pixels.
+static void latlon2pixel( double lat, double lon, int lv, int *px, int *py )
 {
-    latitude = getclip( latitude, LATITUDE_MIN, LATITUDE_MAX );
-    longitude = getclip( longitude, LONGITUDE_MIN, LONGITUDE_MAX );
+    lat = getclip( lat, LATITUDE_MIN, LATITUDE_MAX );
+    lon = getclip( lon, LONGITUDE_MIN, LONGITUDE_MAX );
 
-    double x = ( longitude  + 180) / 360;
-    double sinLatitude = sin( latitude * M_PI / 180);
-    double y = 0.5 - log( ( 1 + sinLatitude ) / ( 1 - sinLatitude ) ) / ( 4 * M_PI );
-    unsigned int mapsize = getmapsize( levelOfDetail );
+    double x = ( lon  + 180) / 360;
+    double sinlat = sin( lat * M_PI / 180 );
+    double y = 0.5 - log( ( 1 + sinlat ) / ( 1 - sinlat ) ) / ( 4 * M_PI );
+    unsigned int mapsize = getmapsize( lv );
 
-    *pixelX = (int)getclip( x * mapsize + 0.5, 0, mapsize - 1 );
-    *pixelY = (int)getclip( y * mapsize + 0.5, 0, mapsize - 1 );
+    *px = (int)getclip( x * mapsize + 0.5, 0, mapsize - 1 );
+    *py = (int)getclip( y * mapsize + 0.5, 0, mapsize - 1 );
 }
 
 
 // Converts a pixel from pixel XY coordinates at a specified level of detail
 // into latitude/longitude WGS-84 coordinates (in degrees).
-// pixelX: X coordinate of the point, in pixels.
-// pixelY: Y coordinates of the point, in pixels.
-// levelOfDetail: Level of detail, from 1 (lowest detail) to 23 (highest detail).
-// latitude: Output parameter receiving the latitude in degrees.
-// longitude: Output parameter receiving the longitude in degrees.
-static void pixelXYToLatLong( int pixelX, int pixelY, int levelOfDetail,
-                              double *latitude, double *longitude )
+// px: X coordinate of the point, in pixels.
+// py: Y coordinates of the point, in pixels.
+// lv: Level of detail, from 1 (lowest detail) to 23 (highest detail).
+// lat: Output parameter receiving the latitude in degrees.
+// lon: Output parameter receiving the longitude in degrees.
+static void pixel2latlon( int px, int py, int lv, double *lat, double *lon )
 {
-    double mapSize = getmapsize( levelOfDetail );
-    double x = ( getclip( pixelX, 0, mapSize - 1 ) / mapSize ) - 0.5;
-    double y = 0.5 - ( getclip( pixelY, 0, mapSize - 1 ) / mapSize );
+    double mapSize = getmapsize( lv );
+    double x = ( getclip( px, 0, mapSize - 1 ) / mapSize ) - 0.5;
+    double y = 0.5 - ( getclip( py, 0, mapSize - 1 ) / mapSize );
 
-    *latitude = 90 - 360 * atan( exp( -y * 2 * M_PI ) ) / M_PI;
-    *longitude = 360 * x;
+    *lat = 90 - 360 * atan( exp( -y * 2 * M_PI ) ) / M_PI;
+    *lon = 360 * x;
 }
 
 
 // Converts pixel XY coordinates into tile XY coordinates of the tile containing
 // the specified pixel.
-// pixelX: Pixel X coordinate.
-// pixelY: Pixel Y coordinate.
-// tileX: Output parameter receiving the tile X coordinate.
-// tileY: Output parameter receiving the tile Y coordinate.
-static void pixelXYToTileXY( int pixelX, int pixelY, int *tileX, int *tileY )
+// px: Pixel X coordinate.
+// py: Pixel Y coordinate.
+// tx: Output parameter receiving the tile X coordinate.
+// ty: Output parameter receiving the tile Y coordinate.
+static void pixel2tile( int px, int py, int *tx, int *ty )
 {
-    *tileX = pixelX / 256;
-    *tileY = pixelY / 256;
+    *tx = px / 256;
+    *ty = py / 256;
 }
 
 
 // Converts tile XY coordinates into pixel XY coordinates of the upper-left pixel
 // of the specified tile.
-// tileX: Tile X coordinate.
-// tileY: Tile Y coordinate.
-// pixelX: Output parameter receiving the pixel X coordinate.
-// pixelY: Output parameter receiving the pixel Y coordinate.
-static void tileXYToPixelXY( int tileX, int tileY, int *pixelX, int *pixelY )
+// tx: Tile X coordinate.
+// ty: Tile Y coordinate.
+// px: Output parameter receiving the pixel X coordinate.
+// py: Output parameter receiving the pixel Y coordinate.
+static void tile2pixel( int tx, int ty, int *px, int *py )
 {
-    *pixelX = tileX * 256;
-    *pixelY = tileY * 256;
+    *px = tx * 256;
+    *py = ty * 256;
 }
 
 
 // Converts tile XY coordinates into a QuadKey at a specified level of detail.
-// tileX: Tile X coordinate.
-// tileY: Tile Y coordinate.
-// levelOfDetail: Level of detail, from 1 (lowest detail) to 23 (highest detail).
+// tx: Tile X coordinate.
+// ty: Tile Y coordinate.
+// lv: Level of detail, from 1 (lowest detail) to 23 (highest detail).
 // returns: A string containing the QuadKey.
-static int tileXYToQuadKey( char *quadkeys, int tileX, int tileY,
-                            int levelOfDetail )
+static int tile2quadkey( char *quadkeys, int tx, int ty, int lv )
 {
-    int i = levelOfDetail;
+    int i = lv;
     char digit = 0;
     int mask = 0;
     int len = 0;
@@ -145,10 +142,10 @@ static int tileXYToQuadKey( char *quadkeys, int tileX, int tileY,
     {
         digit = '0';
         mask = 1 << ( i - 1 );
-        if( ( tileX & mask ) != 0 ){
+        if( ( tx & mask ) != 0 ){
             digit++;
         }
-        if( ( tileY & mask ) != 0 ){
+        if( ( ty & mask ) != 0 ){
             digit += 2;
         }
 
@@ -161,28 +158,27 @@ static int tileXYToQuadKey( char *quadkeys, int tileX, int tileY,
 
 static int encode_lua( lua_State *L )
 {
-    lua_Number latitude = lauxh_checknumber( L, 1 );
-    lua_Number longitude = lauxh_checknumber( L, 2 );
-    lua_Integer levelOfDetail = lauxh_optinteger( L, 3, 23 );
+    lua_Number lat = lauxh_checknumber( L, 1 );
+    lua_Number lon = lauxh_checknumber( L, 2 );
+    lua_Integer lv = lauxh_optinteger( L, 3, 23 );
     char quadkey[23] = {0};
     int len = 0;
-    int pixelX = 0;
-    int pixelY = 0;
-    int tileX = 0;
-    int tileY = 0;
+    int px = 0;
+    int py = 0;
+    int tx = 0;
+    int ty = 0;
 
     lauxh_argcheck(
-        L, levelOfDetail >= 1 && levelOfDetail <= 23, 3,
-        "1-23 expected, got an out of range value"
+        L, lv >= 1 && lv <= 23, 3, "1-23 expected, got an out of range value"
     );
 
     // Converts a point from latitude/longitude WGS-84 coordinates (in degrees)
-    LatLongToPixelXY( latitude,  longitude, levelOfDetail, &pixelX, &pixelY );
+    latlon2pixel( lat,  lon, lv, &px, &py );
     // Converts pixel XY coordinates into tile XY coordinates of the tile
     // containing the specified pixel.
-    pixelXYToTileXY( pixelX, pixelY, &tileX, &tileY );
+    pixel2tile( px, py, &tx, &ty );
     // Converts tile XY coordinates into a QuadKey at a specified level of detail.
-    len = tileXYToQuadKey( quadkey, tileX, tileY, levelOfDetail );
+    len = tile2quadkey( quadkey, tx, ty, lv );
     lua_pushlstring( L, quadkey, len );
 
     return 1;
@@ -191,35 +187,34 @@ static int encode_lua( lua_State *L )
 
 // Converts a QuadKey into tile XY coordinates.
 // quadKey: QuadKey of the tile.
-// tileX: Output parameter receiving the tile X coordinate.
-// tileY: Output parameter receiving the tile Y coordinate.
-// levelOfDetail: Output parameter receiving the level of detail.
-static int quadKeyToTileXY( const char *quadKey, int levelOfDetail, int *tileX,
-                            int *tileY )
+// lv: Output parameter receiving the level of detail.
+// tx: Output parameter receiving the tile X coordinate.
+// ty: Output parameter receiving the tile Y coordinate.
+static int quadkey2tile( const char *quadKey, int lv, int *tx, int *ty )
 {
-    int i = levelOfDetail;
+    int i = lv;
     int mask = 0;
 
-    *tileX = *tileY = 0;
+    *tx = *ty = 0;
     for(; i > 0; i-- )
     {
         mask = 1 << ( i - 1 );
-        switch( quadKey[levelOfDetail - i] )
+        switch( quadKey[lv - i] )
         {
             case '0':
                 break;
 
             case '1':
-                *tileX |= mask;
+                *tx |= mask;
                 break;
 
             case '2':
-                *tileY |= mask;
+                *ty |= mask;
                 break;
 
             case '3':
-                *tileX |= mask;
-                *tileY |= mask;
+                *tx |= mask;
+                *ty |= mask;
                 break;
 
             // Invalid QuadKey digit sequence
@@ -236,29 +231,30 @@ static int decode_lua( lua_State *L )
 {
     size_t len = 0;
     const char *quadkeys = lauxh_checklstring( L, 1, &len );
-    int pixelX = 0;
-    int pixelY = 0;
-    int tileX = 0;
-    int tileY = 0;
-    double latitude = 0;
-    double longitude = 0;
+    int tx = 0;
+    int ty = 0;
 
     lauxh_argcheck(
         L, len >= 1 && len <= 23, 1,
         "length between 1 and 23 expected, got an out of range value"
     );
 
-    if( quadKeyToTileXY( quadkeys, (int)len, &tileX, &tileY ) == 0 ){
+    if( quadkey2tile( quadkeys, (int)len, &tx, &ty ) == 0 ){
+        int px = 0;
+        int py = 0;
+        double lat = 0;
+        double lon = 0;
+
         // Converts tile XY coordinates into pixel XY coordinates of the upper-left
         // pixel of the specified tile.
-        tileXYToPixelXY( tileX, tileY, &pixelX, &pixelY );
+        tile2pixel( tx, ty, &px, &py );
         // Converts a pixel from pixel XY coordinates at a specified level of detail
         // into latitude/longitude WGS-84 coordinates (in degrees).
-        pixelXYToLatLong( pixelX, pixelY, (int)len, &latitude, &longitude );
+        pixel2latlon( px, py, (int)len, &lat, &lon );
 
         lua_createtable( L, 0, 2 );
-        lauxh_pushnum2tbl( L, "lat", latitude );
-        lauxh_pushnum2tbl( L, "lon", longitude );
+        lauxh_pushnum2tbl( L, "lat", lat );
+        lauxh_pushnum2tbl( L, "lon", lon );
         return 1;
     }
 
