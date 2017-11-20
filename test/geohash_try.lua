7d4f5e8942fb490ed114b2c752382ff6c9b6f815
Masatoshi Teruya <mah0x211@gmail.com>
2017-11-20 22:07:18 +0900

    Update test/geohash_try.lua

--- test/geohash_try.lua
+++ test/geohash_try.lua
@@ -5,14 +5,15 @@ local precision = 16;
 local incr = 0.1;
 local LAT_MAX = 90.0;
 local LON_MAX = 180.0;
-local lat, lon, hash, dec;
+local lat, lon, hash, dlat, dlon, err;
 
 for lat = -LAT_MAX, LAT_MAX, incr do
     for lon = -LON_MAX, LON_MAX, incr do
         hash = ifNil( geo.encode( lat, lon, precision ) );
-        dec = ifNil( geo.decode( hash ) );
+        dlat, dlon, err = geo.decode( hash );
+        ifNil( dlat, err );
         -- resolve round-off error
-        ifNotEqual( floor( dec.lat * 1000000 + .5 ), ceil( lat * 1000000 ) );
-        ifNotEqual( floor( dec.lon * 1000000 + .5 ), ceil( lon * 1000000 ) );
+        ifNotEqual( floor( dlat * 1000000 + .5 ), ceil( lat * 1000000 ) );
+        ifNotEqual( floor( dlon * 1000000 + .5 ), ceil( lon * 1000000 ) );
     end
 end
